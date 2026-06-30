48cf6bb73e7636923b62c8c3eaf5f915550f9ca4
Roshan Mallick <roshanmallick2025@gmail.com>
2026-06-30 19:09:21 +0530

    feat: make array deletion interactive with user input

--- 01_arrays/02_delete_element.c
+++ 01_arrays/02_delete_element.c
@@ -2,9 +2,28 @@
 
 int main (){
 
-    int arr[50]={10,20,30,40};
-    int size = 4;
-    int index = 2;
+
+    int size;
+    printf("Enter size of the array : ");
+    scanf("%d",&size);
+
+    int arr[size];
+
+    printf("Enter array elements : ");
+    for (int i = 0 ; i < size ;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int index ;
+
+    printf("Enter index to delete: ");
+    scanf("%d",&index);
+
+    if (index < 0 || index >= size){
+        printf("Enter invalid index \n");
+        return 0;
+    }
+
 
     printf("Before element delete : ");
        for (int i = 0; i < size; i++) {
